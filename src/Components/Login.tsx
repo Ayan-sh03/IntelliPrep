095ee77d4ad03e20953cb92e488789cda35e6278
Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>
2023-08-16 15:44:04 +0530

    Made Books Page Responsive

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useNavigate } from "react-router";
+import NavbarNeo from "./Navbar";
 
 interface loginType {
   username: string;
@@ -53,63 +54,65 @@ export default function Login() {
   }
 
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden font-montserrat ">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-4xl font-semibold text-center text-purple-700 underline">
-          Sign in
-        </h1>
-        <form className="mt-6" onSubmit={handleSubmit}>
-          <div className="mb-2">
-            <label
-              htmlFor="username"
-              className="block text-lg font-semibold text-gray-800 "
+    <>
+      <NavbarNeo />
+      <div className="relative flex flex-col justify-center min-h-screen overflow-hidden font-montserrat ">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+          <h1 className="text-4xl font-semibold text-center text-purple-700 underline">
+            Sign in
+          </h1>
+          <form className="mt-6" onSubmit={handleSubmit}>
+            <div className="mb-2">
+              <label
+                htmlFor="username"
+                className="block text-lg font-semibold text-gray-800 "
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                value={form.username}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="password"
+                className="block text-lg font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <a href="#" className="text-xs text-purple-600 hover:underline">
+              Forget Password?
+            </a>
+            <div className="mt-6">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                Login
+              </button>
+            </div>
+          </form>
+          <p className="mt-8 text-xs font-light text-center text-gray-700">
+            {" "}
+            Don't have an account?{" "}
+            <a
+              href="/signup"
+              className="font-medium text-purple-600 hover:underline"
             >
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              value={form.username}
-              onChange={handleChange}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-lg font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={form.password}
-              onChange={handleChange}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <a href="#" className="text-xs text-purple-600 hover:underline">
-            Forget Password?
-          </a>
-          <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-              Login
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-8 text-xs font-light text-center text-gray-700">
-          {" "}
-          Don't have an account?{" "}
-          <a
-            href="/signup"
-            className="font-medium text-purple-600 hover:underline"
-          >
-            Sign up
-          </a>
-        </p>
+              Sign up
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/Components/Signup.tsx
+++ src/Components/Signup.tsx
@@ -1,6 +1,7 @@
 import { useState, ChangeEvent, FormEvent } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import NavbarNeo from "./Navbar";
 
 interface formType {
   email: string;
@@ -80,76 +81,77 @@ export default function SignUp() {
   //   }
   // };
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden font-montserrat ">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-4xl font-semibold text-center text-purple-700 underline">
-          Sign Up
-        </h1>
-        <form className="mt-6" onSubmit={handleSubmit}>
-          <div className="mb-2">
-            <label
-              htmlFor="email"
-              className="block text-lg font-semibold text-gray-800 "
+    <>
+      <NavbarNeo />
+      <div className="relative flex flex-col justify-center min-h-screen overflow-hidden font-montserrat ">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+          <h1 className="text-4xl font-semibold text-center text-purple-700 underline">
+            Sign Up
+          </h1>
+          <form className="mt-6" onSubmit={handleSubmit}>
+            <div className="mb-2">
+              <label
+                htmlFor="email"
+                className="block text-lg font-semibold text-gray-800 "
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="username"
+                className="block text-lg font-semibold text-gray-800 "
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                value={form.username}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="password"
+                className="block text-lg font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mt-6">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                SignUp
+              </button>
+            </div>
+          </form>
+          <p className="mt-8 text-xs font-light text-center text-gray-700">
+            {" "}
+            Already Registered?{" "}
+            <a
+              href="/login"
+              className="font-medium text-purple-600 hover:underline"
             >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="username"
-              className="block text-lg font-semibold text-gray-800 "
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              value={form.username}
-              onChange={handleChange}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-lg font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              value={form.password}
-              onChange={handleChange}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-
-          <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-              SignUp
-            </button>
-          </div>
-        </form>
-
-        <p className="mt-8 text-xs font-light text-center text-gray-700">
-          {" "}
-          Already Registered?{" "}
-          <a
-            href="/login"
-            className="font-medium text-purple-600 hover:underline"
-          >
-            Sign in
-          </a>
-        </p>
+              Sign in
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
